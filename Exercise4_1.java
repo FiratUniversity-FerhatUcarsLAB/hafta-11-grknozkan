ce7458e5750ace84c811d28f754815d2c946ddc6
Gürkan Özkan <cozkan653@gmail.com>
2025-12-03 08:52:24 +0300

    Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,24 @@
 public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+    public static void printAmerican(String gun, int ay, int tarih, int yil) {
+        System.out.println(gun +", " + ay + " " + tarih + ", " + yil);
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    public static void printEuropean(String gun, int tarih, int ay, int yil) {
+        System.out.println(gun + ", " + tarih + " " + ay + " " + yil);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        String gun = "Thursday";
+        int ay = 11;
+        int tarih = 27;
+        int yil = 2025;
+
+        System.out.println("American Format:");
+        printAmerican(gun, ay, tarih, yil);
+
+        System.out.println("European Format:");
+        printEuropean(gun, tarih, ay, yil);
     }
 }
