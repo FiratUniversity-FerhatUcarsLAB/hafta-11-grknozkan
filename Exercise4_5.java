9b325a08d83d38784aff8ef1b72a87e509158156
Gürkan Özkan <cozkan653@gmail.com>
2025-12-03 09:20:57 +0300

    Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -25,3 +25,30 @@ public class Exercise4_5 {
         System.out.println("any " + strangStrung + "more ");
     }
 }
+
+/*STACK DİYAGRAMI
+Zoop metodu ikinci kez çağrıldığında Stack Diyagramı
+İkinci çağrı şu satırda gerçekleşir:
+zoop("breakfast ", fork);
+
+Bu çağrı clink(4) içinden olur.
+Çağrı yığını tam o anda şöyle görünür:
+┌──────────────────────────────┐
+│ zoop("breakfast ", 4)        │  ← Aktif çalışan metot
+│  • fred = "breakfast "       │
+│  • bob = 4                   │
+├──────────────────────────────┤
+│ clink(4)                     │
+│  • fork = 4                  │
+├──────────────────────────────┤
+│ main(String[] args)          │
+│  • bizz = 5                  │
+│  • buzz = 2                  │
+└──────────────────────────────┘
+*/
+/*
+just for
+any not more 
+It’s breakfast 
+!
+*/
