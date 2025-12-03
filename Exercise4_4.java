fc582469176764e6029f3c4eeeab5fb0a810b399
Gürkan Özkan <cozkan653@gmail.com>
2025-12-03 09:16:47 +0300

    Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,11 +11,31 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        /*
+        Program sorunsuz çalışır
+        Hata vermez
+        Bazı IDE’ler “Unused return value” uyarısı verebilir
+        (örn: “The return value of test() is not used”)
+        */
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        /*
+        System.out.println("boo!") bir void metottur
+        Yani bir değer döndürmez → ifade içinde kullanılamaz 
+
+        Hata mesajı:
+        error: bad operand type void for binary operator '+'
+
+        Açıklama:
+        + operatörü iki operand ister
+        Ama void hiçbir değer değildir
+        Bu yüzden + 7 ile birleştirilemez → tür uyumsuzluğu hatası
+        */
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
 }
+
+
